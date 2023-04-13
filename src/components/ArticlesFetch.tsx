e2a9f74c316bb4a5fb981846d09754d236bdb997
Nestor162 <nestorcicardini@gmail.com>
2023-04-13 16:11:02 +0200

    Fetch articoli

---
+++ src/components/ArticlesFetch.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import { IArticles } from "../interfaces/IArticles";
+import Articles from "./Articles";
+
+function ArticlesFetch() {
+  const API_URL = "https://api.spaceflightnewsapi.net/v4/articles";
+
+  const [articles, setArticles] = useState<IArticles>();
+
+  const articlesFetch = async () => {
+    try {
+      const response = await fetch(API_URL);
+      if (response.ok) {
+        const data = await response.json();
+        setArticles(data);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    articlesFetch();
+  }, []);
+
+  return (
+    <>
+      <h1>Breaking News 🚀</h1>
+      {articles &&
+        articles.results.map(article => {
+          return <Articles article={article} />;
+        })}
+    </>
+  );
+}
+
+export default ArticlesFetch;
