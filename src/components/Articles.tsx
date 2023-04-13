496c75b74247c5280460e3a1b096adaf6dc60ada
Nestor162 <nestorcicardini@gmail.com>
2023-04-13 18:01:07 +0200

    Collegamento a pagina dettagli

--- src/components/Articles.tsx
+++ src/components/Articles.tsx
@@ -2,14 +2,22 @@ import React from "react";
 import { Result } from "../interfaces/IArticles";
 import { Col } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
+import { useNavigate } from "react-router-dom";
 
 interface articleProps {
   article: Result;
 }
+
 function Articles({ article }: articleProps) {
+  const navigate = useNavigate();
   return (
     <Col xs={6} className="mx-auto">
-      <Card className="mb-4">
+      <Card
+        className="mb-4"
+        onClick={() => {
+          navigate(`/details/${article.id}`);
+        }}
+      >
         <Card.Img variant="img-fluid top" src={article.image_url} alt={article.title} />
         <Card.Body>
           <Card.Title>{article.title}</Card.Title>
