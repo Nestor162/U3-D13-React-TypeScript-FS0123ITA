52e8c80421c269396096001c88ffb39100e45714
Nestor162 <nestorcicardini@gmail.com>
2023-04-13 16:44:27 +0200

    Contenuto principale

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import ArticlesFetch from "./components/ArticlesFetch";
+import { Container, Row } from "react-bootstrap";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1 className="text-center mb-4 mt-5">Breaking News 🚀</h1>
+      <Container>
+        <ArticlesFetch />
+      </Container>
+    </div>
+  );
 }
 
 export default App;
